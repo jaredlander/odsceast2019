eb9ef12e53c6a56381a2dcda714cbc7763afe4ab
Jared Lander <info@jaredlander.com>
2019-04-30 10:38:44 -0400

    Showed formulas

--- code/glmnet.r
+++ code/glmnet.r
@@ -19,3 +19,42 @@ house_formula <- TotalValue ~ FireService +
     LotFront + LotDepth + BldgFront + Landmark + 
     BuiltFAR + 
     HistoricDistrict + Built
+
+class(house_formula)
+
+house1 <- lm(house_formula, data=house_train)
+summary(house1)
+coefplot(house1, sort='magnitude')
+
+?glmnet
+
+recipe(
+    house_formula, 
+    data=house_train
+)
+
+
+ny <- tibble::tribble(
+    ~ Boro, ~ Pop, ~ Area, ~ Random,
+    'Manhattan', 1700000, 23, 17,
+    'Queens', 2600000, 104, 42,
+    'Bronx', 1200000, 42, 3,
+    'Staten Island', 475000, 66, 1/2,
+    'Brooklyn', 2400000, 79, pi
+)
+ny
+
+lm(Random ~ Pop, data=ny)
+
+build.x(Random ~ Pop, data=ny)
+
+lm(Random ~ Pop + Area, data=ny)
+build.x(Random ~ Pop + Area, data=ny)
+build.x(Random ~ Pop * Area, data=ny)
+build.x(Random ~ Pop*Area - Pop - Area, ny)
+build.x(Random ~ Pop:Area,ny)
+
+build.x(Random ~ Boro, data=ny)
+build.x(Random ~ Boro, 
+        data=ny,
+        contrasts=FALSE)
